9ad884ce2eba1de2c635d90cab1dc0bd9a44c5e0
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-04-24 21:31:53 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -247,3 +247,18 @@ gfarm:AddParagraph{
     Title = "  ",
     TextSize = 7,
     Content = "O local farm tem que tar la no Dropdown!"
+
+})
+    
+    local Dropdown = main:AddDropdown("CidadeFarm", {
+    Title = "Selecione a Cidade", 
+    Values = {"City", "Snow City", "Magma City", "Legends Highway"},
+    Multi = false,
+    Default = "City"
+})
+
+Dropdown:OnChanged(function(value)
+    Cidade2 = value
+    print("Cidade selecionada:", Cidade2)
+end)
+
